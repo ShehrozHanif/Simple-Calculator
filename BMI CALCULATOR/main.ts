7267ee71f8711fc5ce52047d47cf68e80d871245
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-18 16:08:14 -0700

    added BMICALCULATOR/main.js

---
+++ BMI CALCULATOR/main.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const input1 = await inquirer.prompt({
+    name: "num1",
+    type: "number",
+    message: "Kindly Enter your Height:"
+});
+const input2 = await inquirer.prompt({
+    name: "num2",
+    type: "number",
+    message: "Please Enter your weight in Kg"
+});
+
+let total: number =   input2.num2 / (input1.num1 * input1.num1)
+console.log(`Your BMI is ${total}`);
